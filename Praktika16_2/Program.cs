f55372e46b9cf84a329fd86957882deed442abad
1 <1@DESKTOP-RF1EUM1>
2022-06-13 22:50:01 +0300

    Добавьте файлы проекта.

---
+++ Praktika16_2/Program.cs
@@ -0,0 +1,7 @@
+﻿using Praktika16_2;
+
+Console.WriteLine("Введите текст");
+string[] StringMass = Console.ReadLine().Split(' ', '.', ',');
+Console.WriteLine(Exam.Second(StringMass));
+Console.WriteLine(Exam.Second2(StringMass));
+Console.WriteLine(Exam.Second3(StringMass));
